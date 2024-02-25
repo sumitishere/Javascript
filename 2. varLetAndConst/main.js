f799222bb23711c8f5f727b681f39b42f4ab809e
Sumit Mukharjee <sumitmukharjeeeeee@gmail.com>
2024-02-26 01:09:21 +0530

    Var, let, const, comment and console table output understood

--- 2. varLetAndConst/main.js
+++ 2. varLetAndConst/main.js
@@ -1,15 +1,33 @@
 // Declare Once and can't be changed again - Constant
 // Can be changed in future - variable
-
 const accountId = 56;
 let accountEmail = "sumit@gmail.com"
 var accountPassword = "12345"
 accountCity = "Anuppur"
+let accountState;
 
 
+
+//{
+// js scope were not used in intial days no problems when let were used 
+//}
 // accoundId = 2; can't change as const keyword is there
 console.log(accountId)
-accountEmail = "sumit@hotmail.com"
-accountPassword = "123456"
 
+/*
+  Prefer not to use var
+  becuase of issue in block scope and functional scope
+*/
+
+accountEmail = "sumit@hotmail.com"
+console.log(accountEmail)
+
+accountPassword = "123456"
+console.log(accountPassword)
+
+accountCity = "Jaipur"
+console.log(accountCity)
+
+// a table will be created where we can output our multiple variables simultaneously
+console.table([accountEmail, accountPassword, accountCity, accountState]);
 
